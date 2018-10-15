a3d4c9c21750e530361efb3862b88109388d89f2
Elvira <elvi-ale@hotmail.com>
2018-10-15 16:48:07 +0800

    Removes the negative temperatures

--- Project_New/Task1/FraunchPad_NTC.cpp
+++ Project_New/Task1/FraunchPad_NTC.cpp
@@ -26,13 +26,13 @@
 // Select B57560G1104 for NTC 100 k = 25 oC
 // with temperature scaling = 5 oC, lower limit = -25 oC and upper limit = + 75 oC
 //
-int32_t table[21][2] = {
-  {-25, 1344300},
-  {-20,  998530},
-  {-15,  748670},
-  {-10,  566360},
-  { -5,  432120},
-  {  0,  332400},
+int32_t table[15][2] = {
+ // {-25, 1344300},
+ // {-20,  998530},
+ // {-15,  748670},
+ // {-10,  566360},
+ // { -5,  432120},
+ // {  0,  332400},
   {  5,  257690},
   { 10,  201270},
   { 15,  158340},
@@ -80,7 +80,7 @@ void NTC_FR::celsiusX10(int32_t &t) {
   // Based on table algorithm by larsie — Tue Apr 03, 2012 1:18 pm
   // http://www.43oh.com/forum/viewtopic.php?f=10&p=18608#p18608
   //
-  for (uint8_t i=1; i<21; i++) {
+  for (uint8_t i=1; i<15; i++) {
     if (table[i][1] < _t) {
       t = map(_t, table[i-1][1], table[i][1], 100*table[i-1][0], 100*table[i][0]);
       t /= 10;
@@ -96,7 +96,7 @@ void NTC_FR::fahrenheitX10(int32_t &t) {
   // Based on table algorithm by larsie — Tue Apr 03, 2012 1:18 pm
   // http://www.43oh.com/forum/viewtopic.php?f=10&p=18608#p18608
   //
-  for (uint8_t i=1; i<21; i++) {
+  for (uint8_t i=1; i<15; i++) {
     if (table[i][1] < _t) {
       t = map(_t, table[i-1][1], table[i][1], 100*table[i-1][0], 100*table[i][0]);
       t = (t*9 + 1600) / 50;
